e378fce6f8d1e2f1a363834477fe323cd2c2f800
qianlan <qlhdtly12189@163.com>
2019-08-02 13:34:32 +0800

    rm

--- utils/const.js
+++ utils/const.js
@@ -1,24 +1,3 @@
-
-export var GLOBALDATA = {
-    // baseUrl: 'https://oa.km999.com',
-    cardlist: '',//名片
-    colletelist: [],//名片收藏
-    housearr: [],//楼盘
-    templatecurrnum: 0,//保存名片当前被选中值
-    userInfo: null,//用户基本信息
-    token: '',//token值
-    informations: {},//浏览记录
-    houseid: [],//保存被选中的楼盘ID
-    currentuserinfo: {},
-    visituserinfo: null,
-	header() {
-		return {
-			"X-Requested-With": "XMLHttpRequest",
-			"Authorization": "Bearer " + this.token
-		}
-	}
-};
-
 export const BASE_URL = 'https://dev.km999.com'
 
 export const RULES = {
