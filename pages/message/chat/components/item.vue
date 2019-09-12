b595c4d1bd84838be0885f1aa2d1057442610100
qin <xiouz@163.com>
2019-09-12 17:20:44 +0800

    .

--- pages/message/chat/components/item.vue
+++ pages/message/chat/components/item.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="cu-item self" v-if="bean.send_id == userInfo.id">
+	<view class="cu-item self" v-if="bean.send_id == userInfo.id && bean.send_type==='App\\User'">
 		<view class="main">
 			<view class="content bg-green shadow">
 				<text>{{bean.message.data.content}}</text>
@@ -15,9 +15,16 @@
 				<text>{{bean.message.data.content}}</text>
 			</view>
 			
-			<view style="width: 100%;" class="margin-top-xs" v-if="bean.message.data.contacts && bean.message.data.contacts.length">
-				<text class="text-gray text-sm margin-right-xs">相关人员:</text>
+			<view style="width: 100%;" class="margin-top-xs">
+				<!-- <text class="text-gray text-sm margin-right-xs">相关人员:</text> -->
 				<navigator class="cu-tag bg-cyan radius small" v-for="(i, inx) in bean.message.data.contacts" :key="inx" :url="`/pages/contact/detail/index?id=${i.id}`">@ {{i.name}}</navigator>
+				<navigator
+					class="cu-tag bg-cyan radius small"
+					v-for="(i, inx) in bean.message.data.meta"
+					:key="inx"
+					:url="calcUrl(i)">
+					{{i.title}}
+				</navigator>
 			</view>
 		</view>
 		<view class="date">{{bean.message.created_at|moment('MM-DD HH:mm')}}</view>
@@ -29,11 +36,6 @@
 	import Ava from '@/components/avatar'
 	
 	export default {
-		data() {
-			return {
-				
-			}
-		},
 		props: {
 			bean: Object
 		},
@@ -42,6 +44,18 @@
 		},
 		components: {
 			Ava
+		},
+		methods: {
+			calcUrl(i) {
+				switch(i.type) {
+					case 'customer_sales_pool':
+					case 'customer_sales':
+						return `/pages/customer/detail/index?id=${i.id}&type=新房`
+					case 'customer_distribution_pool':
+					case 'customer_distribution':
+						return `/pages/customer/detail/index?id=${i.id}&type=新房`
+				}
+			}
 		}
 	}
 </script>
