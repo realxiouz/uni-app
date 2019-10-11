fc5aeac04a623a8b64c765ce950681706cb575b8
qin <xiouz@163.com>
2019-10-11 16:05:08 +0800

    customer list

--- pages/customer/index/all.vue
+++ pages/customer/index/all.vue
@@ -11,6 +11,20 @@
 				<input v-model="keywords" :adjust-position="false" type="text" placeholder="姓名 电话搜索" confirm-type="search" @confirm="handleSearch"></input>
 			</view>
 		</view>
+		<view class="flex justify-around bg-white padding-tb-sm">
+			<view @click="handleSort('star')">
+				客户星级
+				<text v-if="sortType=='star'" :class="descending == 'asc' ? 'cuIcon-triangleupfill' : 'cuIcon-triangledownfill'"></text>
+			</view>
+			<view @click="handleSort('owned_at')">
+				添加时间
+				<text v-if="sortType=='owned_at'" :class="descending == 'asc' ? 'cuIcon-triangleupfill' : 'cuIcon-triangledownfill'"></text>
+			</view>
+			<view @click="handleSort('last_genjin_time')">
+				跟进时间
+				<text v-if="sortType=='last_genjin_time'" :class="descending == 'asc' ? 'cuIcon-triangleupfill' : 'cuIcon-triangledownfill'"></text>
+			</view>
+		</view>
 		<swiper style="flex: 1;height: 0;" @change="tabChange" :current="selTab">
 			<swiper-item v-for="(i, inx) in tabs" :key="inx">
 				<data-list :ref="'list'+inx" @data="handleList(inx, $event)" :r-url="i.url" :r-data="i.data">
@@ -94,7 +108,10 @@
 						}
 					},
 				],
-				selTab: 0
+				selTab: 0,
+				
+				sortType: '',
+				descending: 'asc'
 			}
 		},
 		methods: {
@@ -111,14 +128,30 @@
 				this.tabs[this.selTab].data = {...this.tabs[this.selTab].data, keywords: this.keywords}
 			},
 			handleNavChange(inx) {
+				this.reset()
 				this.keywords = ''
+				this.sortType = ''
 				this.selTab = inx
 			},
 			tabChange(e) {
+				this.reset()
 				this.keywords = ''
+				this.sortType = ''
 				this.selTab = e.detail.current
 				this.$refs[`list${this.selTab}`][0].init()
 			},
+			reset() {
+				this.tabs[this.selTab].data = {...this.tabs[this.selTab].data, keywords: ''}
+			},
+			handleSort(sortBy) {
+				if (this.sortType != sortBy) {
+					this.sortType = sortBy
+					this.descending = 'asc'
+				} else {
+					this.descending = this.descending == 'asc' ? 'desc' : 'asc'
+				}
+				this.tabs[this.selTab].data = {...this.tabs[this.selTab].data, sortBy, descending: this.descending}
+			}
 		},
 		components: {
 			Item, DataList, FloatButton
@@ -130,7 +163,7 @@
 			keywords(val) {
 				if (!val) {
 					if (this.tabs[this.selTab].data.keywords) {
-						this.tabs[this.selTab].data = {...this.tabs[this.selTab].data, keywords: ''}
+						this.reset()
 					}
 				}
 			}
